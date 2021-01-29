5e5217151e5109ac4c0edb7d713bca7daf8211de
PUNNAPAT NUANGJAMNONG <punnapat.nuan@bumail.net>
2021-01-29 22:12:00 +0700

    Send AssignMent 1

---
+++ Homework week 1/Assets/Homework.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Homework : MonoBehaviour
+{
+    public string inputformUser;
+    public GameObject input;
+    public GameObject output;
+
+    public void textmacted ()
+    {
+        inputformUser = input.GetComponent<Text>().text;
+        if (inputformUser == "Valorant")
+        {
+            output.GetComponent<Text>().text = "[" + inputformUser + "] is found";
+		}
+        else if (inputformUser == "Point Blank")
+        {
+            output.GetComponent<Text>().text = "[" + inputformUser + "] is found";
+		}
+        else if (inputformUser == "Dead by daylight")
+        {
+            output.GetComponent<Text>().text = "[" + inputformUser + "] is found";
+		}
+        else if (inputformUser == "GTA")
+        {
+            output.GetComponent<Text>().text = "[" + inputformUser + "] is found";
+		}
+        else if (inputformUser == "WWZ")
+        {
+            output.GetComponent<Text>().text = "[" + inputformUser + "] is found";
+		}
+        else
+        {
+            output.GetComponent<Text>().text = "[" + inputformUser + "] not found";
+		}
+	}
+}
